2ef7be25bfd50c23d17697d14ccf340431d8ed30
Mutur1 <muturitrevor1@gmail.com>
2026-01-26 14:41:46 +0300

    fix: small logic correction in the manipulation of for loops

--- section-3/iteration,theforloop.js
+++ section-3/iteration,theforloop.js
@@ -1,21 +1,49 @@
-for (let rep = 7; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep}`);
-}
+// for (let rep = 7; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+
+// const types = [];
+
+// for (let i = 0; i < trevor.length; i++) {
+//     console.log(trevor[i], typeof trevor[i]);
+
+//     types.push(typeof trevor[i]);
+// }
+// console.log(types)
+
+// for(let i=0; i<trevor.length;i++ ){
+//     if(typeof trevor[i] !== 'string')continue
+
+//     console.log(trevor[i], typeof trevor[i]);
+// }
+
+// for (let i = 0; i < trevor.length; i++) {
+//     if (typeof trevor[i] === 'number') break;
+
+//     console.log(trevor[i], typeof trevor[i]);
+// }
+
 
 
 const trevor = [
     'Trevor',
     'Muturi',
-    2025 - 2002,
+    2002,
     'student',
     ['Micheal', 'Peter', 'Steven'],
     true,
-];
-const types = [];
+git];
 
-for (let i = 0; i < trevor.length; i++) {
-    console.log(trevor[i], typeof trevor[i]);
-
-    types.push(typeof trevor[i]);
+for (let i = trevor.length - 1; i >= 0; i--) {
+    console.log(trevor[i]);
 }
-console.log(types)
+
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`------starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Lifting weight repetition ${rep}`)
+    }
+}
