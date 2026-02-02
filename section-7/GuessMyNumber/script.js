891b83a8916e8b481edeb44c2c49b3c8959d9f78
Mutur1 <muturitrevor1@gmail.com>
2026-02-02 10:11:57 +0300

    Finalized GuessMyNumber project with full functionality

--- section-7/GuessMyNumber/script.js
+++ section-7/GuessMyNumber/script.js
@@ -8,8 +8,16 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the score and secretNumber variables
+// 3. Restore the initial conditions of the message, number, score and guess input field
+// 4. Also restore the original background color (#222) and number width (15rem)
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,7 +33,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     score++;
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
+
   // when guess is high
   else if (guess > secretNumber) {
     if (score > 1) {
@@ -48,4 +62,13 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
